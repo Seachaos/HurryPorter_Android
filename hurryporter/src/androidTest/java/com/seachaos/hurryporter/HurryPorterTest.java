c837a6b417081a11b3b3f047b228adec9ebef3a9
Seachaos <ye.jian.yin@gmail.com>
2016-03-01 00:59:48 +0800

    add make request method

--- hurryporter/src/androidTest/java/com/seachaos/hurryporter/HurryPorterTest.java
+++ hurryporter/src/androidTest/java/com/seachaos/hurryporter/HurryPorterTest.java
@@ -3,6 +3,9 @@ package com.seachaos.hurryporter;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
@@ -25,5 +28,36 @@ public class HurryPorterTest extends ApplicationTestCase<Application> {
         String resp = wand.send("http://www.myandroid.tw/test/post.php");
         assertNotNull(resp);
         assertFalse(resp.startsWith(HurryPorter.ERROR_TAG));
+    }
+
+    public void testHurryPorterBasic(){
+        // prepare test data
+        final JSONObject testJSON = new JSONObject();
+        try {
+            testJSON.put("name","test");
+            testJSON.put("int", 1234);
+            testJSON.put("double", 123.456);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // do test
+        HurryPorter porter = new HurryPorter();
+        porter.makeRequestForTest(new HurryPorter.HurryCallback() {
+            @Override
+            public JSONObject prepare(HurryPorter porter) throws JSONException {
+                return testJSON;
+            }
+
+            @Override
+            public void onSuccess(HurryPorter porter, JSONObject json, String raw) {
+                assertTrue(raw!=null);
+            }
+
+            @Override
+            public void onFailed(HurryPorter porter, String raw) {
+                assertFalse(true);
+            }
+        }, "http://www.myandroid.tw/test/post.php");
     }
 }
--- hurryporter/src/main/java/com/seachaos/hurryporter/HurryPorter.java
+++ hurryporter/src/main/java/com/seachaos/hurryporter/HurryPorter.java
@@ -1,7 +1,107 @@
 package com.seachaos.hurryporter;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+
 public class HurryPorter{
     public static final String ERROR_TAG = "[HP_ERR]";
 
     public static String Charset = "UTF-8";
+    private HurryCallback userCallback = null;
+
+    public interface HurryCallback{
+        public JSONObject prepare(HurryPorter porter) throws JSONException;
+        public void onSuccess(HurryPorter porter, JSONObject json, String raw);
+        public void onFailed(HurryPorter porter, String raw);
+    }
+
+    public void makeRequest(final HurryCallback callback, final String url){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                _makeRequest(new HurryCallback() {
+                    @Override
+                    public JSONObject prepare(HurryPorter porter) throws JSONException {
+                        return callback.prepare(porter);
+                    }
+                    @Override
+                    public void onSuccess(HurryPorter porter, JSONObject json, String raw)  {
+                        callback.onSuccess(porter, json, raw);
+                    }
+
+                    @Override
+                    public void onFailed(HurryPorter porter, String raw) {
+                        callback.onFailed(porter, raw);
+                    }
+                }, url);
+            }
+        });
+    }
+
+    public void makeRequestForTest(HurryCallback callback, String url){
+        _makeRequest(callback, url);
+    }
+
+    private void _makeRequest(HurryCallback callback, String url){
+        this.userCallback = callback;
+        HttpWand wand = new HttpWand();
+        JSONObject json = null;
+        try {
+            json = callback.prepare(this);
+        } catch (JSONException e) {
+            onFailed(ERROR_TAG+" prepare data failed:"+e.toString());
+            return;
+        }
+        Iterator<String> keys = json.keys();
+        // prepare datas
+        while(keys.hasNext()){
+            String name = keys.next();
+            try {
+                Object obj = json.get(name);
+                if(obj instanceof String){
+                    String value = (String) obj;
+                    wand.addPost(name, value);
+                }else if(obj instanceof Integer){
+                    int value = (int) obj;
+                    wand.addPost(name, value);
+                }else{
+                    String value = json.getString(name);
+                    wand.addPost(name, value);
+                }
+            } catch (JSONException e) {
+            }
+        }
+        String resp = wand.send(url);
+        if(resp!=null){
+            onReceiveResponse(callback, resp);
+            return;
+        }
+        onFailed(ERROR_TAG + " response is null");
+    }
+
+    private void onReceiveResponse(HurryCallback callback, String resp) {
+        if(userCallback==null){
+            return;
+        }
+        JSONObject json = null;
+        try {
+            json = new JSONObject(resp);
+        } catch (JSONException e) {
+        }
+        callback.onSuccess(this, json, resp);
+    }
+
+    private void onFailed(String reason){
+        if(userCallback==null){
+            return;
+        }
+        userCallback.onFailed(this, reason);
+    }
+
+    public JSONObject getBaseJSON(){
+        return new JSONObject();
+    }
+
 }
